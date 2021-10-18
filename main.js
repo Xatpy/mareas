91322b61af1098c365ab6e540e53716671c5b9d6
Jaime <jaime.chapinal@gmail.com>
2021-10-18 22:26:19 +0200

    Add functionality to calculate tide index

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function loadJSON(callback) {   
-    const urlJson = 'https://raw.githubusercontent.com/Xatpy/mareas/main/noviembre_2020.json';
+function loadJSON(callback) {
+    const urlJson = 'https://raw.githubusercontent.com/Xatpy/mareas/main/octubre_2021.json';
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
     xobj.open('GET', urlJson, true); // Replace 'my_data' with the path to your file
@@ -9,32 +9,57 @@ function loadJSON(callback) {
         callback(xobj.responseText);
     }
     };
-    xobj.send(null);  
+    xobj.send(null);
+}
+
+const getTotalMinutesOfTide = (tide) => {
+    const [tideHour, tideMinutes] = tide.hora.substr(0, tide.hora.indexOf(' ')).split(':')
+    return parseInt(tideHour) * 60 + parseInt(tideMinutes)
+}
+
+const getTideIndex = (currentTime, tides) => {
+    let currentTimeMinutes = parseInt(currentTime.hour) * 60 + parseInt(currentTime.minutes);
+    let arrayMinutes = tides.map(tide => getTotalMinutesOfTide(tide))
+
+    debugger
+    let tideIndex = -1;
+    if (currentTimeMinutes < arrayMinutes[0]) {
+        tideIndex = 0;
+    } else if (currentTimeMinutes > arrayMinutes[arrayMinutes.length - 1]) {
+        tideIndex = arrayMinutes.length - 1;
+    } else {
+        for (let i = 0; i < arrayMinutes.length - 1; ++i) {
+            if (currentTimeMinutes >= arrayMinutes[i] && currentTimeMinutes <= arrayMinutes[i + 1]) {
+                tideIndex = i;
+            }
+        }
+    }
+    return tideIndex;
 }
 
 function init() {
     loadJSON(function(response) {
-    // Parse JSON string into object
+        // Parse JSON string into object
         const actual_JSON = JSON.parse(response);
 
         const mareas = actual_JSON.dias[getDay() - 1].mareas;
 
-        const hour = getHour();
-        const date = getTodayDate() + "   ||   " + hour;
+        let currentTime = getCurrentTime();
+        currentTime = {
+            "hour": "1",
+            "minutes": "39",
+            "seconds": "25"
+        }
+        const tideIndex = getTideIndex(currentTime, mareas);
+        const date = getTodayDate() + "   ||   " ;
+        const time = `${getTodayDate()} || ${currentTime.hour}:${currentTime.minutes}:${currentTime.seconds}`;
         document.getElementById("day").textContent = (date);
-        const time = getHour();
-        //document.write(time);
-
-        let current = 0;
-        
-        for (let i = 0; i < mareas.length; ++i) {
-            document.getElementById("marea" + (i+1).toString()).textContent = mareas[i].hora + " --- " + mareas[i].tipo;
-
-            let d = createDate(mareas[i].hora.split(' ')[0]);
 debugger
-            if (hour < d) {
-                console.log(mareas[i].hora);
-            }
+        let alreadySelected = false;
+        for (let i = 0; i < mareas.length; ++i) {
+            const [tideHour, tideMinutes] = mareas[i].hora.substr(0, mareas[i].hora.indexOf(' ')).split(':')
+            const writeText = mareas[i].hora + " --- " + mareas[i].tipo + (tideIndex === i ? " <----- " : "");
+            document.getElementById("marea" + (i+1).toString()).textContent = writeText;
         }
     });
 }
@@ -49,17 +74,20 @@ function createDate(stringHour) {
     return dateTarget;
 }
 
-function getHour() {
+function getCurrentTime() {
     var today = new Date();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    return time;
+    return {
+        "hour": today.getHours(),
+        "minutes": today.getMinutes(),
+        "seconds": today.getSeconds(),
+    }
 }
 
 function getTodayDate() {
     var today = new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     return date;
-    //return today.getDate();
 }
 
 function getDay() {
